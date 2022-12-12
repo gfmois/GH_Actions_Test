d606c5144ba815537bc267892204358bd40c6681
Moises <daw.moisesguerola@gmail.com>
2022-12-12 20:46:39 +0100

    Jest 3

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-let express = require('express')
+let express = require("express")
 let app = express()
 
 // App Route
-app.get('/', (req, res) => res.send('Hello World'))
+app.get("/", (req, res) => res.send("Hello World"))
 
 app.listen(3000, () => {
-    console.log('Server Listening at port 3000 🚀')
+    console.log("Server Listening at port 3000 🚀")
 })
