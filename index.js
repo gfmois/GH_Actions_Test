8902b8bec5e3a6010a1bda595df10d8120af8b5e
Moises Guerola <daw.moisesguerola@gmail.com>
2023-01-08 18:36:16 +0100

    Telegram Bot - TEST 15

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ const core = require("@actions/core")
 const token = core.getInput("telegram_token")
 const chatID = core.getInput("telegram_id_user")
 
-const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(token, { polling: false });
 
 try {
-    const name = core.getInput("user_name");
-    const message = `Workflow ejecutado correctamente tras el último commit. Saludos ${name}`;
+    const message = `Workflow ejecutado correctamente tras el último commit. Saludos ${core.getInput("user_name")}`;
 
     bot.sendMessage(chatID, message)
-    core.setOutput('msg', "Mensaje enviado correctamente")
+    core.setOutput("msg", "Mensaje enviado correctamente")
 } catch (e) {
     core.setFailed(e.message)
 }
